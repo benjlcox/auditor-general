2ac0f873264ed45fb98fa6219f6d056d7330fbd0
benjlcox <ben@shopify.com>
2015-09-01 18:20:49 +0000

    Improve style, add csv export

--- lib/auditor-general/web.rb
+++ lib/auditor-general/web.rb
@@ -4,6 +4,7 @@ require 'will_paginate/active_record'
 require 'will_paginate/view_helpers/sinatra'
 require "will_paginate-bootstrap"
 require 'erb'
+require 'csv'
 
 module AuditorGeneral
   class Web < Sinatra::Base
@@ -23,21 +24,38 @@ module AuditorGeneral
     set :public_folder, proc { "#{root}/assets" }
     set :views, proc { "#{root}/views" }
 
-    get "/" do
-      @models = AuditorGeneralLog.all.pluck(:model_type).compact.uniq
-      @actions = AuditorGeneralLog.all.pluck(:action).compact.uniq
-      @origins = AuditorGeneralLog.all.pluck(:origin).compact.uniq
-
+    def filter_logs(params)
       result = AuditorGeneralLog.where(nil)
       result = result.filter_model(params[:model])   if params[:model]
       result = result.filter_action(params[:action]) if params[:action]
       result = result.filter_origin(params[:origin]) if params[:origin]
       result = result.start_date(Time.parse(params[:start])) unless params[:start].blank?
       result = result.finish_date(Time.parse(params[:finish])) unless params[:finish].blank?
+      result
+    end
 
-      @logs = result.order(created_at: :desc).paginate(:page => params[:page], :per_page => params[:limit] || 25)
+    get "/" do
+      @models = AuditorGeneralLog.all.pluck(:model_type).compact.uniq
+      @actions = AuditorGeneralLog.all.pluck(:action).compact.uniq
+      @origins = AuditorGeneralLog.all.pluck(:origin).compact.uniq
+
+      result = filter_logs(params)
+
+      @logs = result.order(created_at: :desc).paginate(:page => params[:page], :per_page => params[:limit] || 10)
 
       erb :auditor_general
+    end
+
+    get '/csv' do
+      result = params.empty? ? AuditorGeneralLog.all : filter_logs(params)
+      content_type 'application/csv'
+      attachment "auditor_general_export.csv"
+      csv_string = CSV.generate do |csv|
+        csv << result.column_names
+        result.each do |record|
+          csv << record.attributes.values
+        end
+      end
     end
 
     get "/:model/:id" do
